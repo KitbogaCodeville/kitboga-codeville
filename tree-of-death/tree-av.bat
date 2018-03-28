3806c158d9c195843a2017a7a5625a6c84e76811
Alex Markessinis <markea125@gmail.com>
2018-03-27 21:10:02 -0400

    added tree of death scripts

---
+++ tree-of-death/tree-av.bat
@@ -0,0 +1,43 @@
+::IMPORTANT - for this to work it must be placed in C:\Windows\System32
+::            or somewhere that has been added to Path.
+::
+::            THE REAL TREE (tree.com) MUST ALSO BE RENAMED
+::            TO realtree.com
+::            Created by Discord user - sansfont
+
+@echo off
+realtree.com
+color 02
+echo.
+echo.
+echo.
+echo                                  # #### ####
+echo                                ### \/#^|### ^|/####
+echo                               ##\/#/ \^|^|/##/_/##/_#
+echo                             ###  \/###^|/ \/ # ###
+echo                           ##_\_#\_\## ^| #/###_/_####
+echo                          ## #### # \ #^| /  #### ##/##
+echo                           __#_--###`  ^|{,###---###-~
+echo                                     \ }{
+echo                                      }}{
+echo                                      }}{
+echo                                      {{}
+echo                                , -^=-~{ .-^- _
+echo                                      `}
+echo                                       {
+echo                ---------------------------------
+echo                 ==TREE ANTIWIRUS SCAN RESULTS==
+echo                ---------------------------------
+echo                  NO VIRUSES FOUND
+echo                  NO AD-AWARE FOUND
+echo                  NO MALWARE FOUND
+echo                  NO TROJANS FOUND
+echo                  RETICULATING SPLINES FOUND
+echo                  NO ROOTKITS FOUND
+echo                  NO UPDOGS FOUND
+echo                  NO RANDSOMWARE FOUND
+echo                  NO HACKERS FOUND
+echo                ---------------------------------
+:PauseForever
+pause >nul
+GOTO :PauseForever
---
+++ tree-of-death/tree-picker.ps1
@@ -0,0 +1,10 @@
+# IMPORTANT - for this to work it must be placed in C:\Windows\System32
+#             or somewhere that has been added to Path.
+#
+#             THE REAL TREE (tree.com) MUST ALSO BE RENAMED
+#             TO realtree.com
+#             Created by Discord user - BenWirus
+
+$trees = @('tree-av.bat','tree-inf.bat','tree-bomb.bat');
+$tree = $trees[(Get-Random -Maximum ([array]$trees).count)];
+& $tree;
